cc5c2351026776bd2b356607bd4c76e00952d7ce
SEA1902 <tamthieu1902@gmail.com>
2022-07-12 22:40:02 +0700

    product-page-detail-t3

---
+++ code/Bss/BssProductList/Block/Form.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bss\BssProductList\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Backend\Block\Template\Context;
+
+class Form extends Template
+{
+    public function __construct(Context $context, array $data = [])
+    {
+        parent::__construct($context, $data);
+    }
+}
